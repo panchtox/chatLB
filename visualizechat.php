27161f298818a7db618ffaa11e8252a7f3a29613
Francisco <franaf@gmail.com>
2017-08-21 00:37:07 -0300

    modificado visualizechat con formato compacto de chats

--- visualizechat.php
+++ visualizechat.php
@@ -1,16 +1,23 @@
 <?php
 date_default_timezone_set("America/Argentina/Buenos_Aires");
 $chats_folder="./pruebas/output/";
-$lista_archivos=scandir($chats_folder);
-$chunk_size=5000;
+$lista_archivos=glob($chats_folder."*.txt");
+$archivo=file_get_contents($lista_archivos[max(array_keys($lista_archivos))]);
+preg_match_all('#(\d+\/\d+\/\d+, \d+:\d+ - )(.*)#',$archivo,$match);
+$chat_chunk=$match[0];
 
-for ($i=(sizeof($lista_archivos)-$chunk_size); $i < sizeof($lista_archivos) ; $i++) { 
-	$chat_posts[str_replace(".txt", "", $lista_archivos[$i])]['content']=file_get_contents($chats_folder.$lista_archivos[$i]);
-	$post_parts=preg_split("#\:#", $chat_posts[str_replace(".txt", "", $lista_archivos[$i])]['content']);
-	$chat_posts[str_replace(".txt", "", $lista_archivos[$i])]['author']=$post_parts[0];
-	$chat_posts[str_replace(".txt", "", $lista_archivos[$i])]['content']=preg_replace("/^[\w\W]+:/", "", $chat_posts[str_replace(".txt", "", $lista_archivos[$i])]['content']);
-	$chat_posts[str_replace(".txt", "", $lista_archivos[$i])]['content']=trim($chat_posts[str_replace(".txt", "", $lista_archivos[$i])]['content']);
+foreach ($chat_chunk as $key => $post) {
+	$re="#(\d+\/\d+\/\d+, \d+:\d+ - )#";
+	$chat_posts_pre[$key]=preg_split($re, $post, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+}
 
+foreach ($chat_posts_pre as $key => $value) {
+	$chat_posts[$key]['datetime']=$chat_posts_pre[$key][0];
+	$chat_posts[$key]['datetime']=str_replace(" - ", "", $chat_posts[$key]['datetime']);
+	$chat_posts[$key]['date']=preg_split("#\,\s#", $chat_posts[$key]['datetime'])[0];
+	$post_parts=preg_split("#\:\s#", $chat_posts_pre[$key][1]);
+	$chat_posts[$key]['author']=$post_parts[0];
+	$chat_posts[$key]['content']=$post_parts[1];
 }
 ?>
 <!doctype html>
@@ -20,7 +27,6 @@ for ($i=(sizeof($lista_archivos)-$chunk_size); $i < sizeof($lista_archivos) ; $i
 		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="./styles/lastHundred.css"/>
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
-		<!-- // <script type="text/JavaScript" src="./lib/d3.v2.js"></script> -->
 		<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
 		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
@@ -37,14 +43,13 @@ for ($i=(sizeof($lista_archivos)-$chunk_size); $i < sizeof($lista_archivos) ; $i
 		</div>
 		<script type="text/javascript">
 			var chat_posts_js=<?php echo(json_encode($chat_posts));?>;
-			var formatDateTime=d3.time.format("%y%m%d%H%M").parse;
-			var formatDate=d3.time.format("%y%m%d").parse;
+			var formatDateTime=d3.time.format("%d/%m/%y, %H:%M").parse;
+			var formatDate=d3.time.format("%d/%m/%y").parse;
 			Object.keys(chat_posts_js).forEach(function(k){chat_posts_js[k].datetime=formatDateTime(
-				k.substring(0,6)
-				+''+k.substring(7,11)
+				chat_posts_js[k].datetime
 				)});
 			Object.keys(chat_posts_js).forEach(function(k){chat_posts_js[k].date=formatDate(
-				k.substring(0,6)
+				chat_posts_js[k].date
 				)});
 			chat_posts_array=[];
 			Object.keys(chat_posts_js).forEach(function(k){chat_posts_array.push(chat_posts_js[k])});
@@ -53,7 +58,7 @@ for ($i=(sizeof($lista_archivos)-$chunk_size); $i < sizeof($lista_archivos) ; $i
 
 			var viewportWidth  = document.documentElement.clientWidth
 				,viewportHeight = document.documentElement.clientHeight;
-			var h=viewportHeight*0.30, w=viewportWidth*0.98, margin=30;
+			var h=viewportHeight*0.35, w=viewportWidth*0.98, margin=30;
 			var tooltipHeight=viewportHeight/1.5,tooltipWidth=viewportWidth/3;
 
 			timeline('#chart',nested_data,w,h);
